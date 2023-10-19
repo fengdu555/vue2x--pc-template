709e2abb5216aa1de3b82a9de46150c0c7daf1c2
小伟 <727661683@qq.com>
2023-10-19 11:43:18 +0800

    文案修改-02

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <a-button type="primary"> Primary </a-button>
+    <a-button type="primary"> 按钮啦 </a-button>
   </div>
 </template>
 
